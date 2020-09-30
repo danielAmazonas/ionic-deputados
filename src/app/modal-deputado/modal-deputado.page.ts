7448cc9d5dd8c58819615683ae18f041715a4624
Daniel Souza Amazonas <daniel.amazonas@gmail.com>
2020-09-29 21:31:57 -0300

    Criação do Modal Deputado

--- src/app/modal-deputado/modal-deputado.page.ts
+++ src/app/modal-deputado/modal-deputado.page.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { LoadingController, ModalController } from '@ionic/angular';
+import { Deputado } from '../models/deputado';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-modal-deputado',
@@ -8,10 +10,40 @@ import { ModalController } from '@ionic/angular';
 })
 export class ModalDeputadoPage implements OnInit {
   @Input() id: number;
-  constructor(public modal: ModalController) {}
+  public deputado: Deputado;
+  public carregando: any;
+  public carregamentoFinalizado: boolean = false;
+  public finish: boolean = false;
+
+  constructor(
+    public apiService: ApiService,
+    public modal: ModalController,
+    public loading: LoadingController
+  ) {}
+
+  public buscarDeputado(id: number) {
+    this.showCarregando();
+    this.apiService.getDeputado(id).subscribe((res) => {
+      this.deputado = res.dados;
+      this.fecharCarregando();
+      this.carregamentoFinalizado = true;
+      this.finish = true;
+    });
+  }
+
+  async showCarregando() {
+    this.carregando = await this.loading.create({
+      message: 'Aguarde...',
+    });
+    await this.carregando.present();
+  }
+
+  async fecharCarregando() {
+    await this.carregando.dismiss();
+  }
 
   ngOnInit() {
-    console.log(this.id);
+    this.buscarDeputado(this.id);
   }
 
   public fecharModal(): void {
