45f4b5aed6fc0bb7a5ecaaba9904801052b4307c
Daniel Souza Amazonas <daniel.amazonas@gmail.com>
2020-10-05 19:35:55 -0300

    Bug de inserção corrigido

--- src/app/services/comida.service.ts
+++ src/app/services/comida.service.ts
@@ -6,25 +6,31 @@ import { Storage } from '@ionic/storage';
 })
 export class ComidaService {
   constructor(public storage: Storage) {
-    // storage.set('comidas', [{ nome: 'Pizza' }]);
-    // storage.get('comidas').then((val) => {
-    //   console.log('Comidasss', val);
-    // });
+    // this.removeAll();
   }
 
-  public getAll() {
-    return this.storage.get('comidas').then((comidas) => {
-      if (!comidas) {
-        return [];
-      }
-      return !comidas.isArray ? [] : comidas;
-    });
+  public async getAll() {
+    let comidas = await this.storage.get('comidas');
+    comidas = JSON.parse(comidas);
+    return comidas;
   }
 
-  public salvarComida(comida) {
-    this.getAll().then((data) => {
-      this.storage.set('comidas', data.push(comida));
-      console.log(data);
-    });
+  public async salvarComida(comida) {
+    let comidas = await this.getAll();
+    if (!comidas) {
+      comidas = [];
+    }
+    comidas.push(comida);
+    this.storage.set('comidas', JSON.stringify(comidas));
+  }
+
+  public async removeAll() {
+    await this.storage.remove('comidas');
+  }
+
+  public async remover(id: number) {
+    let comidas = await this.getAll();
+    comidas.splice(id, 1);
+    await this.storage.set('comidas', JSON.stringify(comidas));
   }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { ModalComidaPage } from '../modal-comida/modal-comida.page';
+import { ComidaService } from '../services/comida.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page {
-  constructor(public modal: ModalController) {}
+export class Tab2Page implements OnInit {
+  public comidas: Array<any> = [];
+  constructor(
+    public modal: ModalController,
+    public comidaService: ComidaService
+  ) {}
+
+  ngOnInit(): void {
+    this.getComidas();
+  }
 
   public async abrirModalCadastroComida(id: number) {
     const modal = await this.modal.create({
@@ -18,5 +27,14 @@ export class Tab2Page {
       // },
     });
     return await modal.present();
+  }
+
+  public async getComidas() {
+    this.comidas = await this.comidaService.getAll();
+  }
+
+  public async remover(id: number) {
+    await this.comidaService.remover(id);
+    this.getComidas();
   }
 }
